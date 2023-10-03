0b8a36cb0f4533dfdd79fa9c4245e089efdd34c9
pfremaux <pfremaux.ca@gmail.com>
2023-10-02 20:47:35 -0400

    Removing some example files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -Rf ./build/classes/java/main
-find . -name *.java > sources.txt
+find -path './src/main/*.java' -name *.java > sources.txt
 javac @sources.txt -d ./build/classes/java/main
 
 jar cf server-lib.jar -C build/classes/java/main .
