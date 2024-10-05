9ebc2ee43c7177ab12d079caa58eab695117cbec
pfremaux <pfremaux@gmail.com>
2024-10-05 16:27:44 -0400

    Move cursor in terminal

---
+++ src/example/java/test/CursorTerm.java
@@ -0,0 +1,11 @@
+package test;
+
+public class CursorTerm {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("\u001B[0;0f");
+        System.out.print("Hey");
+    }
+}
